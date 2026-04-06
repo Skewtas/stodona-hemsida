c1c379124d1683d714ae9d4c076a71a8415df2e3
ranishakir-9064 <ranishakir@gmail.com>
2026-04-06 13:15:21 +0200

    Fix React crash by importing QuickBookingWidget

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
+import { QuickBookingWidget } from "../components/QuickBookingWidget";
 import {
   CheckCircle2,
   Star,
